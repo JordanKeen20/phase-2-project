bd7faa359fa4fb3adcac8ff9f25096b21a0a2d09
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-10-27 13:45:20 -0700

    fixed import

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState }from "react";
 import { Switch, Route } from 'react-router-dom';
 import NavBar from "./NavBar";
 import CreateList from "./CreateChore";
-import ExampleList from "./CreateChore";
+import ExampleList from "./ExampleList";
 import IdkList from "./idklist";
 import SpecialAid from "./SpecialAid";
 
@@ -14,16 +14,16 @@ function App() {
     <>
       <NavBar setPageChange = {setPage}/>
         <Switch>
-          <Route path = "specialaid">
+          <Route exact path = "/specialaid">
             <SpecialAid />
           </Route>
-          <Route path = "createlist">
+          <Route exact path= "/createlist">
             <CreateList />
           </Route>
-          <Route path = "examplelist">
+          <Route exact path = "/examplelist">
             <ExampleList />
           </Route>
-          <Route path = "idklist">
+          <Route exact path = "/idklist">
             <IdkList />
           </Route>
         </Switch>
