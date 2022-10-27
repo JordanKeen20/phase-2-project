d57b5d451eb25bfa3980e608f85a7797e1864257
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-10-27 14:12:18 -0700

    current issue

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import { BrowserRouter } from "react-router-dom"
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
 import Index from "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-        <App />
-  </BrowserRouter>
-);
+ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+  document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//         <App />
+//   </BrowserRouter>
+// );
+
+
