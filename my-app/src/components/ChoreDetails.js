e1c7a5b4a8539b01a9bfc141b73f17cbdb9659bf
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-11-15 14:34:49 -0800

    completed

---
+++ my-app/src/components/ChoreDetails.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function ChoreDetails(){
+
+    const [choreList, setChoreList] = useState(null);
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/chores/${id}`)
+        .then(response => response.json())
+        .then(data => setChoreList(data.chores))
+    }, [id])
+
+    if (!choreList) return <h2> wait one moment please...</h2>
+
+    const { name, image, time } = choreList
+
+    
+
+    return(
+        <section>
+            <div className="chore-items">
+                <h1>{name}</h1>
+                <img src= {image} alt="chore picture"/>
+                <div className="time">{time}</div>
+            </div>
+        </section>
+    )
+}
+
+
+export default ChoreDetails;
